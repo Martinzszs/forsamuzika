4b621f02b30a5aac7caa53d220cec53908b92c8d
Martinzszs <114050430+Martinzszs@users.noreply.github.com>
2023-05-10 09:35:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const musicButton = document.querySelector('.js-music')
+
+musicButton.addEventListener('click', () => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley')
+})
